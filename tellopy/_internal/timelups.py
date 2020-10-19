6b2e76cefcd6581fc64ac91bf2449edcf2f8eac8
Your Name <shimizu8981300@gmail.com>
2020-10-20 01:47:36 +0900

    a

--- tellopy/_internal/timelups.py
+++ tellopy/_internal/timelups.py
@@ -1,1 +1,16 @@
 import tellopy
+# tl = tellopy.Tello()
+# tl.connect()
+# tl.takeoff()
+# tl.take_picture()
+# tl.quit()
+
+def take_picture(drone, speed):
+    if speed == 0:
+        return
+    drone.take_picture()
+
+
+    def take_picture(self):
+        log.info('take picture')
+        return self.send_packet_data(TAKE_PICTURE_COMMAND, type=0x68)
